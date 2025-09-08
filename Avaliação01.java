1a6bb4870d235e68a1483da6bd99ca29cb9c0276
Sallyss-yas <thalini.ramos@aluno.edu.pi.gov.br>
2025-09-08 09:56:34 -0300

    Add files via upload

---
+++ Avaliação01.java
@@ -0,0 +1,31 @@
+public class Avaliação01 {
+        public static void main(String[] args) {
+            double n1 = 8.5;
+            double n2 = 5.4;
+            double n3 = 3.4;
+            double n4 = 7.2;
+            double n5 = 10.0;
+            double n6 = 7.7;
+            double n7 = 8.0;
+            double n8 = 6.5;
+
+            double mediaBimestre1 = (n1 + n2) /2;
+            double mediaBimestre2 = (n3 + n4) /2;
+            double mediaBimestre3 = (n5 + n6) /2;
+            double mediaBimestre4 = (n7 + n8) /2;
+
+
+            double semestre1 = (mediaBimestre1 + mediaBimestre2)/2;
+            double semestre2 = (mediaBimestre3 + mediaBimestre4)/2;
+            System.out.println("resultado escolar");
+            System.out.printf("1 bimestre:%.2f/n",mediaBimestre1);
+            System.out.printf("2 bimestre:%.2f/n",mediaBimestre2);
+            System.out.printf("1 semestre: %.2f/n",semestre1);
+
+            System.out.printf("3° bimestre: %.2f/n",mediaBimestre3);
+            System.out.printf("4° bimestre: %.2f/n",mediaBimestre4);
+            System.out.printf("2° semestre: %.2f/n",semestre2);
+
+
+    }
+}
